a133a67f211ae97b761218a695f9bd541a96278d
vnicolau99 <vnicolri7@alumnes.ub.edu>
2025-09-30 08:48:15 +0200

    Modification of the python code to match RoboDK simulation

--- src/python_scripts/Assistive_Feeding_Project.py
+++ src/python_scripts/Assistive_Feeding_Project.py
@@ -1,29 +1,46 @@
+from robodk import robolink, robomath
 import time
-from math import radians, degrees, pi
-import numpy as np
-import socket
-#from spatialmath.base import * 
-from robodk.robolink import *
-from robodk.robomath import *
 
+# Conectar con RoboDK
+RDK = robolink.Robolink()
 
-# Robot setup
-RDK = Robolink()
-robot = RDK.Item("UR5e")
-base = RDK.Item("UR5e Base")
-tool = RDK.Item('Spoon')
-Init_target = RDK.Item('Init')
-Pick_food_target = RDK.Item('Pick_food')
-Move_to_mouth_target = RDK.Item('Move_to_mouth')
-Feed_target = RDK.Item('Feed')
+# Obtener referencias al robot y la herramienta
+robot = RDK.Item('UR5e')
+tool = RDK.Item('Hand')
+robot.setTool(tool)
 
-robot.setPoseFrame(base)
-robot.setPoseTool(tool)
-robot.setSpeed(20)
+# Obtener targets
+init = RDK.Item('Init')
+app_plate = RDK.Item('App_plate')
+pick_food = RDK.Item('Pick_Food')
+out_plate = RDK.Item('Out_plate')
+app_mouth = RDK.Item('App_mouth')
+in_mouth = RDK.Item('In_mouth')
 
-# Robot Constants setup
-ROBOT_IP = '192.168.1.5'
-ROBOT_PORT = 30002
-accel_mss = 1.2
-speed_ms = 0.75
-timel = 4
+# --- PROGRAMA FEEDING ---
+def Feeding():
+    robot.setPoseFrame(RDK.Item('UR5e Base'))   # Sistema de referencia
+    robot.setTool(tool)                         # Herramienta activa
+    
+    robot.MoveJ(init)
+    robot.MoveJ(app_plate)
+    robot.MoveL(pick_food)
+    time.sleep(1.0)  # Pausa 1 s
+    
+    robot.MoveL(out_plate)
+    robot.MoveL(app_mouth)
+    robot.MoveL(in_mouth)
+    time.sleep(3.0)  # Pausa 3 s
+    
+    robot.MoveL(app_mouth)
+    robot.MoveJ(app_plate)
+
+# --- PROGRAMA PRINCIPAL ---
+def MainProgram():
+    robot.setSpeed(600)     # Velocidad en mm/s
+    Feeding()
+    time.sleep(4.0)         # Pausa 4 s
+    robot.MoveJ(init)
+
+# Ejecutar el programa principal
+MainProgram()
